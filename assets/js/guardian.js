020d24288daa5da93818791239095f9e6e42e500
acasallo-presta <acasallo@prestamype.com>
2026-04-20 15:32:24 -0500

    Update guardian.js

--- assets/js/guardian.js
+++ assets/js/guardian.js
@@ -1,4 +1,10 @@
-// Si no hay token en memoria, lo devuelve al inicio inmediatamente
+// 1. GUARDIÁN DE RUTA: Si no hay token en memoria, lo devuelve al inicio
 if (!sessionStorage.getItem('token_google_ia')) {
     window.location.replace('index.html');
+}
+
+// 2. FUNCIÓN DE CERRAR SESIÓN: Disponible para todas las subpáginas
+function cerrarSesion() {
+    sessionStorage.removeItem('token_google_ia'); // Borramos la memoria
+    window.location.replace('index.html'); // Lo redirigimos a la página de login
 }
